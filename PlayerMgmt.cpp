7529ba27bb5a25ba965de1537f90114c59dda20b
mikeysw <2000ysw@naver.com>
2023-04-27 17:01:59 -0400

    Remade structure
    I rebuild the structure and moved all your functions and reset them.

--- PlayerMgmt.cpp
+++ PlayerMgmt.cpp
@@ -1,167 +1,159 @@
-//PlayerMgmt.cpp
+// PlayerMgmt.cpp
 
 #include "PlayerMgmt.h"
-#include <string> 
+
 using namespace std;
 
-void PlayerMgmt::run()
-{
-    entry_list.read_file(file_name);
+void PlayerMgmt::run() {
+    player_list.read_file(pl_file_name);
+    MorS = true; 
     bool done = false;
     while (!done) {
-        display_entry_and_main();
+        if(MorS == true) display_main();
+        else display_searched();
+
         cout << "choice: ";
         char command;
         cin >> command;
-        cin.get(); // new line char
+        cin.get(); 
         execute(command, done);
         cout << endl;
     }
 }
 
-void PlayerMgmt::display_entry_and_main() const {
+void PlayerMgmt::display_main() const {
     string long_separator(50, '-');
     string short_separator(8, '-');
 
     system(clear_command);
 
-    entry_list.display_current_entry();
-
     cout << long_separator << endl
-         << "  (N)ew     (a)dd       (S)earch    \n"
-         << "  (P)rint   (d)isplay   (s)top     \n"
+         << "  (N)ew     (a)dd       (s)earch    \n"
+         << "  (P)rint   (d)isplay   (S)top     \n"
          << short_separator << endl;
 }
-//CHECKKKKKK
 
-void PlayerMgmt::display_entry_and_search() const {
+void PlayerMgmt::display_searched() const {
     string long_separator(50, '-');
     string short_separator(8, '-');
 
     system(clear_command);
 
-    entry_list.display_current_entry();
+    searched_list.display_current_player();
 
     cout << long_separator << endl
-         << "  (S)earch   (n)ext    (p)revious   \n"
-         << "  (e)dit     (P)rint   (E)xit     (s)top     \n"
+         << "  (s)earch   (n)ext     (p)revious   \n"
+         << "  (e)dit     (P)rint    (E)xit     (S)top     \n"
          << short_separator << endl;
 }
 
 void PlayerMgmt::execute(char command, bool & done)
 {
     switch (command) {
-        
         case 'N': {
-            // Start a new season
+            int new_year;
+            cout << "New season: ";
+            cin >> new_year;
+
+            char yorn;
+            cout << "Do you confirm deleting all the existing players and start a new season of " + to_string(new_year) +"? (y or n) ";
+            cin >> yorn;
+
+            if(yorn == 'y'){
+                player_list.set_season(new_year);
+                player_list.clear();
+            }
+            else break;
+
             break;
         }
-        case 'n': {
-            // Move to next
-            entry_list.move_to_next();
-            break;
-        }
-        case 'p': {
-            // Move to previous
-            entry_list.move_to_previous();
-            break;
-        }
-        
         case 'a': {
-            // Add a player
-            PlayerEntry e;
+            //Player e;
+            int category_int;
             string lastName0;
             string firstName0;
-            int yearOfBirth0;
+            string yearOfBirth0;
             string category0;
             string regStatus0;
             cout << "Last name: ";
             getline(cin, lastName0);
-            e.setLastName(lastName0);
+            //e.setLastName(lastName0);
             cout << "First name: ";
             getline(cin, firstName0);
-            e.setLastName(firstName0);
+            //e.setLastName(firstName0);
             cout << "Year of birth: ";       
-            cin >> yearOfBirth0;
-            e.setYearOfBirth(yearOfBirth0);
-            cout << "Category: ";
-            int category_int;                      //we should probably make this into a function
-            category_int = 2023 - int(yearOfBirth0); //season year - birthyear = categoryint
+            getline(cin, yearOfBirth0);
+            //e.setYearOfBirth(yearOfBirth0);
+            category_int = 2023 - stoi(yearOfBirth0); //season year - birthyear = category
             category0 = "U"+ to_string(category_int);
-            e.setCategory(category0);
-            cout << "Registration status (paid or unpaid): ";
+            //e.setCategory(category0);
+            cout << "RegStatus: " << category0;
             getline(cin, regStatus0);
-            e.setLastName(regStatus0);
-            entry_list.add(e);
-            break;
-        }
-        
-        case 'e': {
-            //edit player info
-            if (entry_list.empty())
-                return;
-            PlayerEntry new_data;
-            string lastName0;
-            string firstName0;
-            int yearOfBirth0;
-            string category0;
-            string regStatus0;
-            cout << "Leave blank if no change is needed." << endl;
-            cout << "Update last name: ";
-            getline(cin, lastName0);
-            new_data.setLastName(lastName0);
-            cout << "Update first name: ";
-            getline(cin, firstName0);
-            new_data.setLastName(firstName0);
-            cout << "Update year of birth: ";       
-            cin >> yearOfBirth0;
-            new_data.setYearOfBirth(yearOfBirth0);
-            cout << "Category: ";
-            int category_int;                        //we should probably make this into a function
-            category_int = 2023 - int(yearOfBirth0); //season year - birthyear = categoryint
-            category0 = "U"+ to_string(category_int);
-            new_data.setCategory(category0);
-            cout << "Update registration status (paid or unpaid): ";
-            getline(cin, regStatus0);
-            new_data.setLastName(regStatus0);
+            //e.setLastName(regStatus0);
+            Player e(lastName0, firstName0, yearOfBirth0, category0, regStatus0);
+            player_list.add(e);
+            cout << e.getFirstName() << " " << e.getLastName();
+            cin.get();
+            break;}
+        case 's': {
+            MorS = false;
 
-            entry_list.update_current_player(new_data);
-            break;
-        }
-
-        case 'S': {
-            //Search for players
-            break;
         }
         case 'P': {
-            //Print a list of players
-            /*
-            for(const auto& [e.getLastName(), e.getFirstName(), e.getYearOfBirth(), e.Category(), e.RegStatus()] : entry_list){
+            /*for(const auto& [e.getLastName(), e.getFirstName(), e.getYearOfBirth(), e.Category(), e.RegStatus()] : player_list){
                 cout << e.lastName << '\n';
                 cout << e.firstName << '\n';
                 cout << e.yearOfBirth << '\n';
                 cout << e.category << '\n';
                 cout << e.regStatus << '\n';
-            }
-            */
+            }*/
+            player_list.display_current_player();
+            
             break;
         }
-        case 'd': {
-            //Display Statistics
-            /*
-            PlayerEntry::display_stats();
+        case 'd': {}
+        case 'S': {}
 
-            //blah blah
-            */
-
+        case 'n': {
+            searched_list.move_to_next();
             break;
         }
-        case 's': {
-            // Stop the program
-            entry_list.write_file(file_name);
-            done = true;
+        case 'p': {
+            searched_list.move_to_prev();
             break;
         }
-        
+        case 'e': {
+            //Player e;
+            int category_int;
+            string lastName0;
+            string firstName0;
+            string yearOfBirth0;
+            string category0;
+            string regStatus0;
+            cout << "Last name: ";
+            getline(cin, lastName0);
+            //e.setLastName(lastName0);
+            cout << "First name: ";
+            getline(cin, firstName0);
+            //e.setLastName(firstName0);
+            cout << "Year of birth: ";       
+            getline(cin, yearOfBirth0);
+            //e.setYearOfBirth(yearOfBirth0);
+            category_int = 2023 - stoi(yearOfBirth0); //season year - birthyear = category
+            category0 = "U"+ to_string(category_int);
+            //e.setCategory(category0);
+            cout << "RegStatus: " << category0;
+            getline(cin, regStatus0);
+            //e.setLastName(regStatus0);
+            Player e(lastName0, firstName0, yearOfBirth0, category0, regStatus0);
+            player_list.update_current_player(e);
+            cout << e.getFirstName() << " " << e.getLastName();
+            cin.get();
+            break;
+        }
+        case 'E': {
+            searched_list.clear();
+            MorS = true;
+        }
     }
 }
--- PlayerMgmt.h
+++ PlayerMgmt.h
@@ -8,24 +8,27 @@
 #include <map>
 #include <string>
 
-#include "PlayerMgmtList.h"
-#include "PlayerEntry.h"
+#include "PlayerList.h"
+#include "Player.h"
 
 //const char clear_command[] = "cls"; // for Windows
 const char clear_command[] = "clear"; // for Linux and possibly Mac's
 
-const char file_name[] = "playerlist.txt";
+const char pl_file_name[] = "playerlist.txt";
 
-class PlayerMgmt{
+class PlayerMgmt {
 public:
     void run();
 
 private:
-    void display_entry_and_main() const; // main view - before searching (return to this view with command exit)
-    void display_entry_and_search() const; // second view - after searching
+    void display_main() const;
+    void display_searched() const;
     void execute(char command, bool & done);
 
-    PlayerMgmtList entry_list;
+    bool MorS; // true:main false:search
+
+    PlayerList player_list;
+    PlayerList searched_list;
 };
 
 #endif
